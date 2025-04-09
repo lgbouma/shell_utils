65c6e8e75ce8da7ded1523600889ddf696f0fe98
lgbouma <bouma.luke@gmail.com>
2025-04-09 10:31:43 -0700

    update quality_flag_decoder

--- quality_flag_decoder.sh
+++ quality_flag_decoder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Description:
-#   This script takes a 12-bit quality flag integer as input and performs the following:
+#   This script takes a 13-bit quality flag integer as input and performs the following:
 #   1. Prints the binary representation of the quality flag.
 #   2. Prints the bits that are set (true) in the quality flag along with their corresponding description.
 #
@@ -9,14 +9,14 @@
 #   ./quality_flag_decoder.sh <quality_flag>
 #
 # Parameters:
-#   quality_flag: An integer representing the 12-bit quality flag.
+#   quality_flag: An integer representing the 13-bit quality flag.
 #
 # Example:
 #   ./quality_flag_decoder.sh 1028
 #   Output:
-#     Binary representation: 010000000100
-#     Bit 10 is on (S21 CP/CB)
-#     Bit 2 is on (M_G <3.9)
+#     Binary representation: 0010000000100
+#     Bit 2 is on (M_G<3.9 or M_G>8.5)
+#     Bit 10 is on (Metallicity outlier)
 
 # Check if the quality flag argument is provided
 if [ $# -eq 0 ]; then
@@ -28,7 +28,7 @@ fi
 quality_flag=$1
 
 # Convert the quality flag to binary representation
-binary_representation=$(printf "%012d" $(echo "obase=2; $quality_flag" | bc))
+binary_representation=$(printf "%013d" $(echo "obase=2; $quality_flag" | bc))
 
 # Print the binary representation
 echo "Binary representation: $binary_representation"
@@ -45,12 +45,13 @@ bit_strings=(
     "RUWE>1.4"
     "Crowded"
     "Far from main-sequence"
+    "Metallicity outlier"
     "S21 CP/CB"
     "P_rot not in the homogeneous S19 S21 sample"
 )
 
 # Iterate over each bit and print the bits that are set (true) along with their description
-for ((i=0; i<12; i++)); do
+for ((i=0; i<13; i++)); do
     bit=$((quality_flag & (1 << i)))
     if [ $bit -ne 0 ]; then
         echo "Bit $i is on (${bit_strings[i]})"
